884966d67678ae3a89d49f3999f16977f0897953
Sbonelo2 <cbonelo224@gmail.com>
2025-11-06 16:42:52 +0200

    localhost4040

--- config/database.ts
+++ config/database.ts
@@ -1,7 +1,5 @@
 import { Pool } from "pg";
 import dotenv from "dotenv";
-
-
 dotenv.config();
 const pool = new Pool({
   user: process.env.DB_USER,
@@ -10,9 +8,7 @@ const pool = new Pool({
   password: process.env.DB_PASSWORD,
   port: parseInt(process.env.DB_PORT || "5432"),
 });
-
 export const query = (text: string, params?: any[]) => pool.query(text, params);
-
 export const testDbConnection = async () => {
   try {
     const client = await pool.connect();
---
+++ server.ts
@@ -0,0 +1,64 @@
+require("dotenv").config();
+import { Express, Request, Response, NextFunction } from "express";
+import { testDbConnection } from "./config/database";
+import path from "path";
+
+
+const express = require("express");
+const app: Express = express();
+app.use(express.json());
+// serve static asset from public
+app.use(express.static(path.join(__dirname, "public")));
+app.get("/", (req, res) => {
+  req: Request;
+  res: Response;
+  res.sendFile(path.join(__dirname, "views", "index.html"));
+});
+
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.status(404).json({
+    success: false,
+    message: "Route not found",
+  });
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
+});
+testDbConnection();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import express from "express";
+// import dotenv from "dotenv";
+// import { testDbConnection } from "./config/database";
+// dotenv.config();
+// const app = express();
+// const PORT = process.env.PORT || 5000;
+// const startServer = async () => {
+//   await testDbConnection();
+//   app.use(express.json());
+//   // testDbConnection();
+//   app.listen(PORT, () => {
+//     console.log(`Server is running on http://localhost:${PORT}`);
+//   });
+// };
+// startServer();
